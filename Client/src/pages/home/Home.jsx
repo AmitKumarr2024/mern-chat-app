556f13733b54ad1f22d5624ea286f6697d08c7bb
Amit kumar <codes.amitkumar@gmail.com>
2024-08-24 00:42:51 +0530

    socket url set-2

--- Client/src/pages/home/Home.jsx
+++ Client/src/pages/home/Home.jsx
@@ -4,7 +4,7 @@ import MessageContainer from "../../components/messages/MessageContainer";
 
 const Home = () => {
   return (
-    <div className=" flex sm:h-[550px] md:h-[600px] rounded-lg overflow-hidden bg-gray-500 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+    <div className=" flex sm:h-[550px] md:h-[600px] rounded-lg md:overflow-y-scroll overflow-hidden bg-gray-500 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
       <Sidebar />
       <MessageContainer/>
     </div>
